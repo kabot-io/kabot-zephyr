87f96c43801adafdcb1b2b0a0f521c55a87a524a
Krzysztof Pochwała <k.pochwala@gmail.com>
2025-09-07 23:39:08 +0200

    feat: add clangd, clang-format, cmake-format tools, and build tasks

--- scripts/build.zsh
+++ scripts/build.zsh
@@ -1,4 +1,4 @@
 #!/usr/bin/zsh
+west build app --build-dir build/native_sim -b native_sim -- -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
-west build app --build-dir build/native_sim -b native_sim
-west build app --build-dir build/esp32s3_devkitc -b esp32s3_devkitc/esp32s3/appcpu
+west build app --build-dir build/esp32s3_devkitc -b esp32s3_devkitc/esp32s3/appcpu -- -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
