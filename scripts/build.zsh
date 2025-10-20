68c88dab5c16a9f3b65596b53ec7d3e9c4392029
Krzysztof Pochwała <k.pochwala@gmail.com>
2025-10-20 16:15:34 +0200

    feat: minimal working build

--- scripts/build.zsh
+++ scripts/build.zsh
@@ -3,8 +3,10 @@
 CURRENT_DIR=$(pwd)
 cd $(git rev-parse --show-toplevel)
 
-west build app --build-dir build/native_sim -b native_sim -p -- -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+source $(find /opt/ros/*/setup.zsh)
+source .venv/bin/activate
 
-west build app --build-dir build/esp32s3_devkitc -b esp32s3_devkitc/esp32s3/appcpu -p -- -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+west build app --build-dir build/native_sim -b native_sim -- -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+west build app --build-dir build/esp32s3_devkitc -b esp32s3_devkitc/esp32s3/appcpu -- -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 cd $CURRENT_DIR
