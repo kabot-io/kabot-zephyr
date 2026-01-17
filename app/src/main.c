39d753f651f39a432897f0aa702ce0495a8fd1b9
Krzysztof Pochwała <k.pochwala@gmail.com>
2026-01-17 12:11:25 +0000

    feat: working udp listener od port 3000

--- app/src/main.c
+++ app/src/main.c
@@ -1,12 +1,24 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
-int main(void)
-{
-    int counter = 0;
+#include <zephyr/net/socket.h>
+
+int main(void) {
+    int sock = zsock_socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(3000),
+        .sin_addr.s_addr = INADDR_ANY,
+    };
+
+    zsock_bind(sock, (struct sockaddr *)&addr, sizeof(addr));
+
+    char buf[64];
     while (1) {
-        printk("%d\n", counter++);
-        k_sleep(K_MSEC(1000));
+        int len = zsock_recv(sock, buf, sizeof(buf) - 1, 0);
+        if (len > 0) {
+            buf[len] = '\0';
+            printk("Received: %s\n", buf);
+        }
     }
-    return 0;
 }
