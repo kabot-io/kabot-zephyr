5b96f11f156d50ebc40c587c37e81a9576846e97
Krzysztof Pochwała <k.pochwala@gmail.com>
2026-01-26 21:38:19 +0100

    feat: working udp bridge from ros2 to servos

--- app/src/main.c
+++ app/src/main.c
@@ -3,6 +3,7 @@
 #include <zephyr/zbus/zbus.h>
 
 #include "motor_service.h"
+#include "zbus/effort_subscriber.h"
 #include "zbus/effort_msg.h"
 #include "zbus/effort_channel.h"
 
@@ -11,26 +12,6 @@
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
-
-
-ZBUS_SUBSCRIBER_DEFINE(effort_subscriber, 4);
-static void effort_subscriber_task(void)
-{
-    const struct zbus_channel *chan;
-    LOG_INF("Starting subscriber on effort channel");
-    while (!zbus_sub_wait(&effort_subscriber, &chan, K_FOREVER)) {
-        if (&effort_channel != chan) {
-            continue;
-        }
-        struct effort_msg effort;
-        if (zbus_chan_read(&effort_channel, &effort, K_MSEC(20)) == 0) {
-            LOG_INF("From subscriber -> Left effort=%d, Right effort=%d", effort.left, effort.right);
-        } else {
-            LOG_WRN("Failed to read from effort_channel");
-        }
-    }
-}
-K_THREAD_DEFINE(effort_subscriber_task_id, CONFIG_MAIN_STACK_SIZE, effort_subscriber_task, NULL, NULL, NULL, 3, 0, 0);
 
 
 static void autoconnect_wifi(void)
@@ -54,6 +35,7 @@ static void autoconnect_wifi(void)
 static int kabot_init(void)
 {
     autoconnect_wifi();
+    initialize_motor_pwms();
     return start_motor_service();
 }
 
@@ -62,7 +44,28 @@ SYS_INIT(kabot_init, APPLICATION, 99);
 int main(void)
 {
     // High-level application logic goes here
+    LOG_INF("Main loop running");
+
+    struct effort_msg forward = {
+        .left = 100,
+        .right = 100
+    };
+
+    struct effort_msg stop = {
+        .left = 0,
+        .right = 0
+    };
+
+    struct effort_msg backward = {
+        .left = -100,
+        .right = -100
+    };
+
     while (1) {
         k_sleep(K_FOREVER);
+        // set_motor_effort(&forward);
+        // k_sleep(K_MSEC(3000));
+        // set_motor_effort(&stop);
+        // k_sleep(K_MSEC(500));
     }
 }
