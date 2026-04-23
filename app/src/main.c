9df0f378d2de33d6b5a9ce8a16befc6bbfeb3b51
Krzysztof Pochwała <k.pochwala@gmail.com>
2026-04-23 20:20:03 +0000

    chore: add tio to devcontainer, cleanup main, add flashing to build script

--- app/src/main.c
+++ app/src/main.c
@@ -9,10 +9,8 @@
 
 #include <zephyr/net/wifi_mgmt.h>
 
-
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
-
 
 static void autoconnect_wifi(void)
 {
@@ -21,7 +19,7 @@ static void autoconnect_wifi(void)
     struct net_if *iface = net_if_get_wifi_sta();
 
 #ifdef CONFIG_WIFI_NM_WPA_SUPPLICANT_CRYPTO_ENTERPRISE
-	wifi_set_enterprise_credentials(iface, 0);
+    wifi_set_enterprise_credentials(iface, 0);
 #endif /* CONFIG_WIFI_NM_WPA_SUPPLICANT_CRYPTO_ENTERPRISE */
 
     int rc = net_mgmt(NET_REQUEST_WIFI_CONNECT_STORED, iface, NULL, 0);
@@ -46,26 +44,7 @@ int main(void)
     // High-level application logic goes here
     LOG_INF("Main loop running");
 
-    struct effort_msg forward = {
-        .left = 100,
-        .right = 100
-    };
-
-    struct effort_msg stop = {
-        .left = 0,
-        .right = 0
-    };
-
-    struct effort_msg backward = {
-        .left = -100,
-        .right = -100
-    };
-
     while (1) {
         k_sleep(K_FOREVER);
-        // set_motor_effort(&forward);
-        // k_sleep(K_MSEC(3000));
-        // set_motor_effort(&stop);
-        // k_sleep(K_MSEC(500));
     }
 }
--- scripts/build.zsh
+++ scripts/build.zsh
@@ -8,5 +8,6 @@ source .venv/bin/activate
 
 # west build app --build-dir build/native_sim -b native_sim -- -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 west build app --build-dir build/esp32s3_devkitc -b esp32s3_devkitc/esp32s3/procpu -- -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+# west flash --build-dir build/esp32s3_devkitc
 
 cd $CURRENT_DIR
