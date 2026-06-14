0a7aa868e947363ff23a80fd4fb0b15b3be295f2
Krzysztof Pochwała <k.pochwala@gmail.com>
2026-06-14 19:27:13 +0200

    feat: check if necessary tools are installed

--- scripts/format.zsh
+++ scripts/format.zsh
@@ -1,5 +1,13 @@
 #!/bin/env zsh
 
+# Check for required tools
+for tool in clang-format shellcheck ruff fdfind ; do
+    if ! command -v "$tool" &> /dev/null; then
+        echo "Error: $tool is not installed" >&2
+        exit 1
+    fi
+done
+
 REPO_ROOT=$(git rev-parse --show-toplevel)
 cd "${REPO_ROOT}" || exit
 
